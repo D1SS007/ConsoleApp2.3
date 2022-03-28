2f20efd9a77e0eff8e36e01eb5c9b5ce5995085e
Alex Zverev <lordzver007@yandex.ru>
2022-03-28 16:37:43 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2.3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp2._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startNumber = 5;
+            int endNumber = 96;
+            int step = 7;
+
+            for (int i = startNumber;  i <= endNumber; i += step)
+            {
+                Console .Write(i + " ");
+            }
+        }
+    }
+}
